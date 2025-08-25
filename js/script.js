4f1c308c53a1b05a7a787ef258b234df5949dc8f
alaa351 <aljundialaa327@gmail.com>
2025-08-25 06:11:59 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,22 +2,44 @@ let sidbar_toog = document.querySelector("#sidbar-toog");
 let sidepar = document.getElementById("sidepar");
 let content = document.querySelector("#content");
  
-sidbar_toog.addEventListener("click" , () => {
+// sidbar_toog.addEventListener("click" , () => {
 
-    let sidbarwidth = window.getComputedStyle(sidepar).width;
-    if (sidbarwidth == "250px"){
-        sidepar.style.width ="0px";
+//     let sidbarwidth = window.getComputedStyle(sidepar).width;
+//     if (sidbarwidth == "250px"){
+//         sidepar.style.width ="0px";
 
-        content.style.marginRight ="0px";
+//         content.style.marginRight ="0px";
 
-    }
-    else{
-        sidepar.style.width ="250px";
-        content.style.marginRight="250px";
-    }});
+//     }
+//     else{
+//         sidepar.style.width ="250px";
+//         content.style.marginRight="250px";
+//     }});
     	
 
+sidbar_toog.addEventListener("click" , () => { 
+  let sideparRight =  window.getComputedStyle(sidepar).right;
+  if(window.innerWidth > 767){
+    if(sideparRight == "0px"){
+      sidepar.style.right ="-250px"; 
+       content.style.marginRight = "0";
 
+    }
+    else {
+      sidepar.style.right ="0"; 
+       content.style.marginRight = "250px";
+
+    }
+  }
+  else{
+    if( sideparRight == "0px"){
+       sidepar.style.right ="-250px";
+    }
+    else{
+       sidepar.style.right ="0";
+    }
+  }
+});
 
 
 new DataTable('#example', {
